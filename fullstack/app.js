7c80ba6ff7c5bfbed7d363fae34ab7689df00110
finger92 <zhouyi920521@gmail.com>
2016-03-22 13:13:11 -0400

    fix access control

--- fullstack/app.js
+++ fullstack/app.js
@@ -54,14 +54,10 @@ app.use(session({
 var config = require('./config');
 var _ = require('lodash');
 
-var allowCrossDomain = function(req, res, next) {
-    res.header("Access-Control-Allow-Credentials", true);
-}
-
 app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
     res.header("Access-Control-Allow-Origin", req.headers.origin);
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, authorization,X-Prototype-Version,Allow,*, Content-Length");
-    res.header("Access-Control-Allow-Credentials", true);
     res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS, HEAD");
     next();
 });
