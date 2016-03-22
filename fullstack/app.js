fe7fb6187d238d5af435b70e692fb7eec6e21a86
finger92 <zhouyi920521@gmail.com>
2016-03-22 16:39:49 -0400

    fix

--- fullstack/app.js
+++ fullstack/app.js
@@ -33,7 +33,7 @@ app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(_id_dirname, 'public')));
+app.use(express.static(path.join(__dirname, 'public')));
 app.use(session({
     secret: config.session_secret,
     store: new MongoStore({
