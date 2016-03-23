7ebbcaad3156cdfc24fff3a946d78e1c5c763d92
EC2 Default User <ec2-user@ip-172-31-33-118.us-west-2.compute.internal>
2016-03-23 03:43:26 +0000

    fix

--- fullstack/app.js
+++ fullstack/app.js
@@ -48,7 +48,8 @@ app.use(session({
 }));
 
 app.use(function(req, res, next) {
-    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Origin", req.headers.origin);
+    res.header("Access-Control-Allow-Credentials", true);
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, authorization,X-Prototype-Version,Allow,*, Content-Length");
     res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS, HEAD");
     next();
